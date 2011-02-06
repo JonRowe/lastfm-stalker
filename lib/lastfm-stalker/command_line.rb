9168ae717fe6750da21d2c4730d440716569afd0
Jon Rowe <hello@jonrowe.co.uk>
2011-02-06 22:25:24 +0000

    initial command line behaviour

--- lib/lastfm-stalker/command_line.rb
+++ lib/lastfm-stalker/command_line.rb
@@ -1,6 +1,11 @@
 module Lastfm
   module Stalker
-    class CmdLine
+    class CommandLine
+      def self.process(args)
+        client = Client.new
+        client.set_user args[1]
+        $stdout.puts client.now_playing
+      end
     end
   end
 end
--- spec/lastfm-stalker/command_line_spec.rb
+++ spec/lastfm-stalker/command_line_spec.rb
@@ -1,7 +1,34 @@
 require 'spec_helper'
 
 module Lastfm::Stalker
-  describe CmdLine do
-    subject { CmdLine::process '--user a_user' }
+  describe CommandLine do
+    let!(:client) { mock "client" }
+    let(:args) { ['--user','a_user'] }
+    let(:now_playing) { 'a_user is listening to blahblah by the yackety yacks' }
+
+    before do
+      Client.stub(:new).and_return client
+      client.stub(:set_user)
+      client.stub(:now_playing).and_return now_playing
+    end
+
+    subject { CommandLine::process args }
+
+    it "should create a new client" do
+      Client.should_receive(:new).and_return client
+      subject
+    end
+    it "should setup the user" do
+      client.should_receive(:set_user).with('a_user')
+      subject
+    end
+    it "should find out now playing" do
+      client.should_receive(:now_playing).and_return(now_playing)
+      subject
+    end
+    it "should return nowplaying to stdout" do
+      $stdout.should_receive(:puts).with(now_playing)
+      subject
+    end
   end
 end
