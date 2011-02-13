53b9017247f13e09f9426338f3bdd984a66f307f
Jon Rowe <hello@jonrowe.co.uk>
2011-02-13 17:57:51 +0000

    command line steps

--- features/step_definitions/cmd_line_steps.rb
+++ features/step_definitions/cmd_line_steps.rb
@@ -1,3 +1,13 @@
 When /^I run$/ do |command|
-  run_simple unescape command
+  # reset arguments to the ones we wish to supply
+  ARGV.clear
+  command.strip.split.each { |i| ARGV << i }
+  executable = "./bin/#{ARGV.shift}"
+
+  # simulate running command, we only do this to keep webmock stubs intact
+  if File.executable? executable
+    load executable
+  else
+    raise "Command not executable"
+  end
 end
