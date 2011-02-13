8c9b99a8d61bb19b09689a03fc2cd48d9994b8d4
Jon Rowe <hello@jonrowe.co.uk>
2011-02-13 18:01:59 +0000

    client stuff

--- lib/lastfm-stalker/client.rb
+++ lib/lastfm-stalker/client.rb
@@ -1,6 +1,24 @@
 module Lastfm
   module Stalker
     class Client
+      include HTTParty
+
+      attr_reader :user
+
+      base_uri "http://ws.audioscrobbler.com/2.0"
+
+      def api_key
+        'b25b959554ed76058ac220b7b2e0a026'
+      end
+
+      def set_user(value)
+        @user = value
+      end
+
+      def now_playing
+        query = { user: user, format: 'json', api_key: self.api_key, method: 'user.getrecenttracks' }
+        Client.get self.class.base_uri, query: query
+      end
     end
   end
 end
--- spec/lastfm-stalker/client_spec.rb
+++ spec/lastfm-stalker/client_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 
 module Lastfm::Stalker
   describe Client do
+
+    describe "class attributes" do
+      subject { Client }
+
+      its(:base_uri) { should == "http://ws.audioscrobbler.com/2.0" }
+    end
+
+    its(:'class.ancestors') { should include HTTParty }
+    its(:api_key) { should == 'b25b959554ed76058ac220b7b2e0a026' }
+
     describe "set_user" do
       let(:user) { "a_user" }
 
@@ -9,5 +19,27 @@ module Lastfm::Stalker
 
       its(:user) { should == user }
     end
+
+    describe "now_playing" do
+      let(:user) { "a_user" }
+      let(:client) { Client.new }
+
+      before do
+        client.set_user user
+      end
+
+      subject { client.now_playing }
+
+      it "should get the user you wish to stalk" do
+        client.should_receive(:user).and_return(user)
+        subject
+      end
+      it "should use HTTParty to get the most recent tracks for the user" do
+        query = hash_including format: 'json', api_key: client.api_key, method: 'user.getrecenttracks', user: user
+        Client.should_receive(:get).with(Client.base_uri,query: query)
+        subject
+      end
+    end
+
   end
 end
