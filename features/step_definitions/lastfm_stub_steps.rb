ece306ffe6d603b05d9c5b547398516616a1eb12
Jon Rowe <hello@jonrowe.co.uk>
2011-02-13 21:30:25 +0000

    webmock was sensitive to this

--- features/step_definitions/lastfm_stub_steps.rb
+++ features/step_definitions/lastfm_stub_steps.rb
@@ -10,7 +10,7 @@ Given /^there is a last fm user "([^"]*)" currently listening to "([^"]*)" by "(
       }
     }
   JSON
-  uri = "http://ws.audioscrobbler.com/2.0/"
+  uri = "http://ws.audioscrobbler.com/2.0"
   method = 'user.getrecenttracks'
   api_key = 'b25b959554ed76058ac220b7b2e0a026'
   stub_request(:get, uri).with( query: { method: method, user: user, format: 'json', api_key: api_key } ).to_return( body: json, status: 200 )
