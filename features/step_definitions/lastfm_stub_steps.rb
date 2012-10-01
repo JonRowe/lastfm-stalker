8634f08bd78aff02bc02e43e13004ef44440d438
Jon Rowe <hello@jonrowe.co.uk>
2012-10-01 21:27:51 +0100

    syntax

--- features/step_definitions/lastfm_stub_steps.rb
+++ features/step_definitions/lastfm_stub_steps.rb
@@ -13,7 +13,5 @@ Given /^there is a last fm user "([^"]*)" currently listening to "([^"]*)" by "(
   uri = "http://ws.audioscrobbler.com/2.0"
   method = 'user.getrecenttracks'
   api_key = 'b25b959554ed76058ac220b7b2e0a026'
-  stub_request(:get, uri)
-    .with( :query => { :method => method, :user => user, :format => 'json', :api_key => api_key } )
-    .to_return( :body => json, :status => 200, :headers => { "Content-type" => 'application/json' } )
+  stub_request(:get, uri).with( :query => { :method => method, :user => user, :format => 'json', :api_key => api_key } ).to_return( :body => json, :status => 200, :headers => { "Content-type" => 'application/json' } )
 end
